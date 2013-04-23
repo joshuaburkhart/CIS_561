17df3395c2b30ccabed3e334d67fd523ba5edb40
Joshua Burkhart <jburkhar@uoregon.edu>
2013-04-23 09:29:10 -0700

    ..

--- hw2/run.sh
+++ hw2/run.sh
@@ -6,14 +6,14 @@
 #Example: ./run.sh cool_examples/A2I.cool
 
 java -jar beaver-cc.jar -a cool.grammar \
-\
-jflex lexer_spec.jflex &&               \
-\
+&& \
+jflex lexer_spec.jflex                  \
+&& \
 javac -cp beaver-rt-0.9.11.jar:.        \
 Lexer4Cool.java                         \
 Parser4Cool.java                        \
 cool/Terminals.java                     \
 cool/ErrorReport.java                   \
-Parser.java &&                          \
-\
+Parser.java                             \
+&& \
 java -classpath beaver-rt-0.9.11.jar:. Parser $1
